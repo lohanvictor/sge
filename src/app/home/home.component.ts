87bd65a5afbd3e33b5e313dbd183f7e7964365d5
evaldoes <evaldopkscardoso@gmail.com>
2021-06-14 14:44:25 -0400

    sge

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    public router: Router
+    ) { }
+
+  ngOnInit(): void {
+  }
+
+  select(op: string) {
+    if(op == 'sgf') {
+      document.location.href='https://sge.uea.edu.br/sgf'
+    } else {
+      document.location.href='https://sge.uea.edu.br/sgo'
+    }
+  }
+}
